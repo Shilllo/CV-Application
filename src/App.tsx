c882f13d6a5cf6a37c8e037c4d234858d1a5a23a
shillo <kimsemen0402@mail.ru>
2024-09-28 12:05:09 +0300

    Add style

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
 
+import "./App.css";
+import EditSide from "./components/edit-side/edit-side";
+import ResumeContainer from "./components/resume-container/resume-container";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="App-main">
+        <EditSide />
+        <ResumeContainer />
+      </main>
     </div>
   );
 }
